7c8fc8180d8f2d47dea13d005b5405fcbf5bab4c
Mariyam Shahid <merriam.shahid@gmail.com>
2022-12-15 11:20:31 +0500

    first time commiting

--- indexpython/gitty.py
+++ indexpython/gitty.py
@@ -1,1 +1,22 @@
 print("heelo")
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver= webdriver.Chrome(ChromeDriverManager().install())
+driver.get("http://10.4.4.20:6121/login")
+time.sleep(2)
+driver.find_element(By.XPATH,"//input[@id='login-email']").send_keys('luke@gmail.com')
+time.sleep(2)
+driver.find_element(By.XPATH,"//input[@placeholder='Enter Password']").send_keys('123')
+driver.find_element(By.XPATH,"//span[@class='p-button-label']").click()
+#//input[@placeholder='Enter Password']
+time.sleep(10)
+driver.find_element(By.XPATH,"//a[@class='toogle-icon full-content']")
+driver.find_element(By.CLASS_NAME,'paragrapgh-text-3 active')
+time.sleep(10)
+#//p[@class='paragraph-text-3 active']
+
